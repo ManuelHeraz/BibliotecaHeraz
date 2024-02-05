a1ec26817effb2d4c8dba409f9f169442a2fa999
ManuelHeraz <hernandezdiazjm00@gmail.com>
2024-02-05 16:24:25 -0600

    v2.0

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
-window.onload = function(){
-    document.querySelector(".loader").style.opacity = 0;
-    setTimeout(() => {
-        document.querySelector(".loader").style.display = "none"
-    }, 1000);
-}
+$(document).ready(function(){
+    $('.filter-item').click(function(){
+        const value = $(this).attr('data-filter')
+        if (value == 'all'){
+            $('.post-box').show('1000')
+        }
+        else{
+            $('.post-box')
+                .not('.' + value)
+                .hide('1000')
+            $('.post-box')
+                .filter('.' + value)
+                .show('1000')
+        }
+    });
+
+    $('.filter-item').click(function(){
+        $(this).addClass('active-filter').siblings().removeClass('active-filter');
+    });
+});
+
+let header = document.querySelector('header')
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow',window.scrollY > 50);
+});
