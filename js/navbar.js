02cdde32736aab7695e990ab3b50320230aa49be
ManuelHeraz <hernandezdiazjm00@gmail.com>
2024-03-26 19:50:56 -0600

    v24.03.26.0750

---
+++ js/navbar.js
@@ -0,0 +1,12 @@
+let menuList = document.getElementById("menuList")
+menuList.style.maxHeight = "0px";
+
+function toggleMenu(){
+    if(menuList.style.maxHeight == "0px")
+    {
+        menuList.style.maxHeight = "300px"
+    }
+    else{
+        menuList.style.maxHeight = "0px"
+    }
+}
